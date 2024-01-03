19d81c232a9bc0f3f959a6d01f5fc3a449b627d4
william <william@williams-MacBook-Pro.local>
2024-01-03 09:39:44 +0100

    date format

--- app/monitor/[userbranch]/columns.tsx
+++ app/monitor/[userbranch]/columns.tsx
@@ -34,9 +34,9 @@ export const columns: ColumnDef<monitoType>[]=[
   },
   {
     header:"DATE",
-    accessorKey:"createdAt",
+    accessorKey:"createdAt_In",
     cell:({row})=>{
-      const createdAt = row.getValue("createdAt");
+      const createdAt = row.getValue("createdAt_In");
       const formatted  = new Date(createdAt as string)
       return <div className="font-medium">
         {/* {formatted} */}
